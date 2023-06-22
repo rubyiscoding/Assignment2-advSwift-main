21888e208ac23330421a195699d9c53d5b7aaa07
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-06-21 21:22:37 -0400

    Added parent class and subclass.

--- Assignment2-advSwift/Question3.swift
+++ Assignment2-advSwift/Question3.swift
@@ -24,8 +24,40 @@ import Foundation
  - turnOn() => will set the state to 'On'
  - turnOff() => will set the state to 'Off'
  - getTemp() -> Double
- 
- 
+ */
+
+class Computer {
+    var cpu: String
+    var gpu: String
+    var ram: Int
+    var state: String = "Off"
+    var temp: Double
+    
+    init(cpu: String, gpu: String, ram: Int, temp: Double) {
+        self.cpu = cpu
+        self.gpu = gpu
+        self.ram = ram
+        self.temp = temp
+    }
+    
+    func upgradeRam(newAmount: Int) {
+        ram = newAmount
+    }
+    
+    func turnOn() {
+        state = "On"
+    }
+    
+    func turnOff() {
+        state = "Off"
+    }
+    
+    func getTemp() -> Double {
+        return temp
+    }
+}
+
+ /**
  CHILD CLASS:
  class name:
  - Laptop
@@ -38,3 +70,27 @@ import Foundation
  - override upgradeRam() => if the ram is not upgradable do nothing
  - closeScreen() => this will set the laptop state to 'Sleep'
  */
+
+class Laptop: Computer {
+    var isRamUpgradable: Bool
+    var screenSize: Double
+    var isTouchScreen: Bool
+    
+    init(cpu: String, gpu: String, ram: Int, state: String, temp: Double, isRamUpgradable: Bool, screenSize: Double, isTouchScreen: Bool) {
+        self.isRamUpgradable = isRamUpgradable
+        self.screenSize = screenSize
+        self.isTouchScreen = isTouchScreen
+        super.init(cpu: cpu, gpu: gpu, ram: ram, temp: temp)
+        self.state = state
+    }
+    
+    override func upgradeRam(newAmount: Int) {
+        if isRamUpgradable {
+            ram = newAmount
+        }
+    }
+    
+    func closeScreen() {
+        state = "Sleep"
+    }
+}
