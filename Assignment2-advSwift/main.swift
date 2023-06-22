cf3d555ebb8aa0fd072c5d2f42bc241f6b3663bb
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-06-21 21:06:31 -0400

    run func1 and func2

--- Assignment2-advSwift/main.swift
+++ Assignment2-advSwift/main.swift
@@ -9,3 +9,24 @@ import Foundation
 
 print("Hello, World!")
 
+
+//Function 1
+let height = 5
+let width = 5
+
+let area = calculateArea(height: height, width: width)
+print("The area of the square is: \(area)")
+
+
+//Function 2
+let string1 = "kamal"
+let string2 = allCap(string: string1)
+print("The letters after converting are: \(string2)")
+
+
+//Question 2
+
+let rectangle = Rectangle(height: 5, width: 8, color: "green")
+print("Perimeter: \(rectangle.getPerimeter())")
+rectangle.printColor()
+
